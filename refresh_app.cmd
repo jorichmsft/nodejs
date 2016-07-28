3e19d359afe898285a680f9b9860267302883ff9
John Richardson (BASE OS) <jorich@microsoft.com>
2016-07-28 15:08:55 -0700

    add refresh_app, add a name to container run

---
+++ refresh_app.cmd
@@ -0,0 +1,32 @@
+
+rem
+rem Refresh the application images and container after update.
+rem
+
+rem
+rem Remove the application container if present
+rem
+
+docker rm httpclient1
+
+rem
+rem Remove the image if present
+rem
+
+docker rmi httpclient
+
+rem
+rem Now rebuild the image
+rem
+
+cd httphello
+docker build -t httphello .
+cd ..
+
+rem
+rem Now create and launch a new container on the updated application image.
+rem
+
+docker run --name httphello1 -p 8080:8080/tcp -it httphello
+
+
--- run_app.cmd
+++ run_app.cmd
@@ -3,6 +3,6 @@ rem
 rem Run the application from the created image.
 rem
 
-docker run -p 8080:8080/tcp -it httphello
+docker run --name httphello1 -p 8080:8080/tcp -it httphello
 
 rem start iexplore.exe http://localhost:8080
